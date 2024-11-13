b5b660233155b307fe0b7fbf3f5970882514a2d3
Maxime Girard <maxime.girard.cpro@gmail.com>
2024-11-13 13:41:55 +0100

    feat(OC-7): finished implementing values for User Dto

--- app/src/main/java/com/openclassrooms/arista/data/entity/UserDto.kt
+++ app/src/main/java/com/openclassrooms/arista/data/entity/UserDto.kt
@@ -1,6 +1,21 @@
 package com.openclassrooms.arista.data.entity
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity(tableName = "user")
-data class UserDto()
+data class UserDto(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    val id: Long = 0,
+
+    @ColumnInfo(name = "name")
+    val name: String,
+
+    @ColumnInfo(name = "email")
+    val email: String,
+
+    @ColumnInfo(name = "password")
+    val password: String
+)
